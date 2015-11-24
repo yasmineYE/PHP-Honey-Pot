a0f16b8d930495c912cd6669a7c5b4bfe192e047
KevinBacas <bacas.kevin@hotmail.fr>
2015-11-24 17:56:45 +0100

    Added an installation script

---
+++ install_db.php
@@ -0,0 +1,50 @@
+<?php
+
+// Database architecture
+// # main
+// - users
+//   - id
+//   - login
+//   - password
+
+
+$sql_users = <<<SQL
+  CREATE TABLE users
+  (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    login varchar(255),
+    password varchar(255)
+  );
+SQL;
+
+$sql_list = <<<SQL
+  CREATE TABLE list
+  (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title varchar(255),
+    date varchar(255),
+    content varchar(255)
+  );
+SQL;
+
+$conn = require_once('./mysql_connect.php');
+$res = $conn->query("DROP DATABASE main;");
+$res = $conn->query("CREATE DATABASE main;");
+$conn->select_db("main");
+if($res) {
+  $res = $conn->query($sql_users);
+  if($res) {
+    $res = $conn->query($sql_list);
+    if($res) {
+      echo "Installation done.";
+    } else {
+      echo "Cannot create list table : ".$conn->error;
+    }
+  } else {
+    echo "Cannot create users table : ".$conn->error;
+  }
+} else {
+  echo "Cannot create database named 'main'\n".$conn->error;
+}
+
+?>
